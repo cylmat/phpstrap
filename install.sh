11d6d531b5119c5b3af2caf97daa665c229f70e2
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-04-19 21:37:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Download PHP servers Docker containers
-curl -fLo ./ https://github.com/cylmat/phpserver.git
+git clone --depth=1 https://github.com/cylmat/phpserver.git .
 
 # Download home config with custom prompts and editor 
-curl -fLo ./.docker/.home https://github.com/cylmat/homeconfig
+git clone --depth=1 https://github.com/cylmat/homeconfig ./.docker/.home
 
 # Download PHP environment with specs and tests frameworks
-env git clone --depth=1 https://github.com/cylmat/phpconfig.git .
+git clone --depth=1 https://github.com/cylmat/phpconfig.git .
